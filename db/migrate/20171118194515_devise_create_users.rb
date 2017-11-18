f4197fc2e88e14ec1f34d86460ecb8c82044309d
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2017-11-19 06:02:12 +0900

    migration files

--- db/migrate/20171118194515_devise_create_users.rb
+++ db/migrate/20171118194515_devise_create_users.rb
@@ -1,13 +1,15 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+      t.string :username
+
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      # t.string :email,              null: false, default: ""
+      # t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      # t.string   :reset_password_token
+      # t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
@@ -34,9 +36,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,         unique: true
+    # add_index :users, :email,                unique: true # Database authenticatable
+    # add_index :users, :reset_password_token, unique: true # Recoverable
+    # add_index :users, :confirmation_token,   unique: true # Confirmable
+    # add_index :users, :unlock_token,         unique: true # Lockable
   end
 end
