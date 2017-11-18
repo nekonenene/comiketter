954c50c12856ecc7c022b9cbc2b816b79da90b82
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2017-11-19 06:07:53 +0900

    deviseとomniauthでcallbackを受け取れるところまで

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,24 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :rememberable, :trackable, :timeoutable, :omniauthable, :omniauth_providers => [:twitter]
+
+  class << self
+    # 認証情報を元にユーザー検索、存在しなければ作る
+    def from_omniauth(auth)
+      find_or_create_by(provider: auth["provider"], uid: auth["uid"]) do |user|
+        user.provider = auth["provider"]
+        user.uid = auth["uid"]
+        user.username = auth["info"]["nickname"]
+      end
+    end
+
+    def new_with_session(params, session)
+      if session["devise.user_attributes"]
+        new(session["devise.user_attributes"]) do |user|
+          user.attributes = params
+        end
+      else
+        super
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "home#index"
 
+  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
   resources :auth, only: [:destroy], format: false
   get "auth/:provider/new", to: "auth#new", format: false, as: :auth_new
   get "auth/:provider/callback", to: "auth#create", format: false
