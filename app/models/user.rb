8ae840ebbb272b2300aef3440b2926944617afff
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2017-11-19 08:58:13 +0900

    Twitterアカウントでユーザー作成・ログインができるように

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   class << self
     # 認証情報を元にユーザー検索、存在しなければ作る
     def from_omniauth(auth)
+      puts "1############################################"
+      puts auth
+      puts "############################################"
       find_or_create_by(provider: auth["provider"], uid: auth["uid"]) do |user|
         user.provider = auth["provider"]
         user.uid = auth["uid"]
@@ -12,6 +15,9 @@ class User < ApplicationRecord
     end
 
     def new_with_session(params, session)
+      puts "2############################################"
+      puts params
+      puts "############################################"
       if session["devise.user_attributes"]
         new(session["devise.user_attributes"]) do |user|
           user.attributes = params
