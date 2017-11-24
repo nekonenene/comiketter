72db94dacd5794d2426ea17b7b52de249796db74
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2017-11-24 16:34:58 +0900

    Set timezone for DB

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module Comiketter
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ja
+    config.active_record.default_timezone = :local
+    config.time_zone = "Tokyo"
   end
 end
