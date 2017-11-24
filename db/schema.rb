a1185c9b4c8ddcde743256263e27c56318334d5a
ハトネコエ (nekonenene) <hato.nekoe@gmail.com>
2017-11-24 18:28:33 +0900

    セッション管理をActiveRecordでおこなうように

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,15 @@
 
 ActiveRecord::Schema.define(version: 0) do
 
+  create_table "sessions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", comment: "for activerecord-session_store" do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
+  end
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
     t.string "handle", collation: "utf8mb4_general_ci", comment: "メンションに使われるID"
     t.string "username"
